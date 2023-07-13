0efcad92f120d62ecebf64db9e329c09704a23a3
cristi <cristilive6@gmail.com>
2023-07-13 11:40:00 -0700

    working basic use info

--- app/src/main/java/com/example/appdevfinalprojct2/landing_page.java
+++ app/src/main/java/com/example/appdevfinalprojct2/landing_page.java
@@ -2,13 +2,65 @@ package com.example.appdevfinalprojct2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class landing_page extends AppCompatActivity {
+    Spinner spinner;
+
+    private static String TAG = landing_page.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+
+
+
+
+
+
+
+
+
+
+
+        //----------------- Navigation need on all pages-------
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.navigation, android.R.layout.simple_spinner_dropdown_item );
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(TAG, "intent is "+parent.getSelectedItem());
+
+                Intent intent=null;
+                String destination= parent.getSelectedItem().toString();
+
+                if(destination.equalsIgnoreCase("Landing Page")){
+                    intent= new Intent(landing_page.this, landing_page.class);
+                }else if(destination.equalsIgnoreCase("User Basic Info")){
+                    intent= new Intent(landing_page.this, user_basicinfo.class);
+                }
+
+
+
+
+                startActivity(intent);
+
+                //        Log.v(TAG, "intent is "+spinner.getSelectedItem());
+
+            }@Override public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+
+
     }
 }
