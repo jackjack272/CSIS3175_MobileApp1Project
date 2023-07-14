988e80930475d39c113f459bfddc2a188d9a15af
cristi <cristilive6@gmail.com>
2023-07-13 17:25:01 -0700

    layout for the recipe is set up

--- app/src/main/java/com/example/appdevfinalprojct2/Get_Recipies.java
+++ app/src/main/java/com/example/appdevfinalprojct2/Get_Recipies.java
@@ -34,8 +34,10 @@ public class Get_Recipies extends AppCompatActivity {
     private RequestQueue requestQueue;
 
     private ArrayList<Dish> dishArrayList;
+    // api call components
 
-    private static String TAG= Get_Recipies.class.getSimpleName();
+
+    private static String TAG = Get_Recipies.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,86 +50,81 @@ public class Get_Recipies extends AppCompatActivity {
 
 
 
-        recyclerView= findViewById(R.id.recipee_destination);
-        layoutManager= new LinearLayoutManager(this);
+
+
+
+
+
+        // on click send to another page with intent and do api call and display on that page.
+
+
+        recyclerView = findViewById(R.id.recipee_destination);
+        layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        requestQueue=VollySingolton.getInstance(this).getRequestQueue();
-        dishArrayList=new ArrayList<>();
+        requestQueue = VollySingolton.getInstance(this).getRequestQueue();
+        dishArrayList = new ArrayList<>();
         getRecipee();
-
     }
 
-    private void getRecipee (){
+    private void getRecipee() {
 //                String cusineStyle, String diet, int numDishes, int maxCalories,
 //                             String ingrident1, String ingrident2,  String ingrident3  ){
 
-        String url="https://api.spoonacular.com/recipes/complexSearch?apiKey=3e14d423c5b54657a99a4fa84e8d3905" +
+        String url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=3e14d423c5b54657a99a4fa84e8d3905" +
                 "&cuisine=italian&diet=vegetarian&query=rice&maxCalories=2400&number=3&sourceUrl";
 
-
-
-
-        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
 //                        https://stackoverflow.com/questions/1568762/accessing-members-of-items-in-a-jsonarray-with-java
-                        try{
-                            JSONArray  response_array=response.getJSONArray("results");
-                            for(int i=0; i<response_array.length(); i++){
+                try {
+                    JSONArray response_array = response.getJSONArray("results");
+                    for (int i = 0; i < response_array.length(); i++) {
 
-                                JSONObject jsonObject= response_array.getJSONObject(i);
+                        JSONObject jsonObject = response_array.getJSONObject(i);
 
-                                JSONArray xx= jsonObject.getJSONObject("nutrition").getJSONArray("nutrients");
-                                JSONObject z;
-                                double calories=0;
-                                for(int a=0; a<xx.length(); a++){
-                                    z= xx.getJSONObject(i);
-                                    Log.v(TAG, z.getString("amount"));
-                                    calories= z.getDouble("amount");
-                                }
-
-                                Dish dish= new Dish(
-                                        jsonObject.getInt("id"),
-                                        jsonObject.getString("title"),
-                                        jsonObject.getString("image"),
-                                        calories
-                                );
-//                                Log.v(TAG, String.valueOf("***************** DISH OBJ TO ARRAY ************************** "));
-//                                Log.v(TAG, String.valueOf(dish));
-                                dishArrayList.add(dish);
-
-                            }
-                        }catch (Exception e){
+                        JSONArray xx = jsonObject.getJSONObject("nutrition").getJSONArray("nutrients");
+                        JSONObject z;
+                        double calories = 0;
+                        for (int a = 0; a < xx.length(); a++) {
+                            z = xx.getJSONObject(i);
+                            Log.v(TAG, z.getString("amount"));
+                            calories = z.getDouble("amount");
                         }
 
-
-                        // Reccle adapter comes here.
-                        RecipeeAdapter recipeeAdapter= new RecipeeAdapter( dishArrayList, Get_Recipies.this );
-                        recyclerView.setAdapter(recipeeAdapter);
+                        Dish dish = new Dish(
+                                jsonObject.getInt("id"),
+                                jsonObject.getString("title"),
+                                jsonObject.getString("image"),
+                                calories
+                        );
+//                                Log.v(TAG, String.valueOf("***************** DISH OBJ TO ARRAY ************************** "));
+//                                Log.v(TAG, String.valueOf(dish));
+                        dishArrayList.add(dish);
 
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                    }
-                });
+                } catch (Exception e) {
+                }
 
+
+                // Reccle adapter comes here.
+                RecipeeAdapter recipeeAdapter = new RecipeeAdapter(dishArrayList, Get_Recipies.this);
+                recyclerView.setAdapter(recipeeAdapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
 
         Log.v(TAG, String.valueOf("************* DISH ARRAY ****************************** "));
         Log.v(TAG, String.valueOf(dishArrayList));
 
         requestQueue.add(request);
-
-
-
-
     }
-
-
-
-
 
 
 }
